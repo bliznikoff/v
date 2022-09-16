571f9bd6c4122a6e597b06b59d5cb1b0a1183b7f
bliznikov <224@ngs.ru>
2022-09-16 18:13:29 +0300

    Ok

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,4 +1,24 @@
-﻿
-Console.WriteLine(" 2 Hello, World!");
-Console.WriteLine(" 2 Hello, World!");
-Console.WriteLine(" 2 Hello, World!");
+﻿/* 
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine(" Hello, Geek!");
+Console.WriteLine("Введите значение a");
+int userNumerA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(userNumerA);
+Console.WriteLine("Введите значение b");
+int userNumerB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(userNumerB);
+if (userNumerA > userNumerB)
+{
+   Console.WriteLine("max");
+    Console.WriteLine(userNumerA);
+}
+else
+{
+    Console.WriteLine("max");
+    Console.WriteLine(userNumerB);
+}
