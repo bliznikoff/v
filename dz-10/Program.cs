5f20c3ccec2a7e3d1170e97f65f25630cc5563ae
bliznikov <224@ngs.ru>
2022-09-24 15:40:58 +0300

    commit

---
+++ dz-10/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+
+int numberDigit(int numberX)
+  {
+        int  n10 = (int)Math.Log10(numberX) + 1;
+    return  n10;
+  }
+
+int digit2number(int numberX)
+{
+  int digit2 = numberX / 10 % 10;
+  return digit2;
+}
+
+Console.WriteLine("Привет, Дмитрий Соломонов!");
+Console.WriteLine("Введите значение числа");
+int numberX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numberX);
+
+int n10 = numberDigit(numberX);
+if  (n10 == 3)
+{
+Console.WriteLine(n10);
+}
+else
+{
+  Console.WriteLine("Число не трёхзначное!!!");
+}
+
+int digit2 = digit2number(numberX);
+Console.WriteLine($"Второй знак в числе [{numberX}] {digit2}");
+
+
