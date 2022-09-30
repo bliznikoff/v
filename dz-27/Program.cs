378b11023d573140fa8b1a21e11300dce3ffa97e
bliznikov <224@ngs.ru>
2022-09-30 23:31:40 +0300

    27 ok

---
+++ dz-27/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/ 
+Console.Clear();
+Console.WriteLine("Приветствую! Эта программа принимает на вход число и выдаёт сумму цифр в числе.");
+Console.WriteLine("Введите значение числа A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы ввели число A равное {numberA}");
+int sumCharNumber = 0;
+
+while (numberA > 0)
+{
+    int C = 0;   
+     C = numberA % 10;
+     sumCharNumber += C;
+    numberA = numberA /10;
+}  
+Console.WriteLine($"Сумма чисел равна {sumCharNumber}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
