da089d553253e84f5f1669cd9bc28c1a0ab578b8
bliznikov <224@ngs.ru>
2022-10-20 17:28:08 +0300

    dz-64 ok

---
+++ dz-64/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+ в промежутке от N до 1. Выполнить с помощью рекурсии.
+*/ 
+
+void RangePrint(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    Console.WriteLine($"{N}, ");
+    RangePrint(N-1);
+}
+
+
+Console.Clear();
+Console.WriteLine("Приветствую! Эта программа выведет все натуральные числа в промежутке от N до 1.");
+Console.WriteLine("Введите N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы ввели  число равное {numberN}");
+RangePrint(numberN);
